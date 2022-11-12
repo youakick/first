88e5c9c0217514225fe9d6174a88afb46e8686d9
Vasyl <example@gmail.com>
2022-11-12 16:30:06 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/* alert('hello'); */
+
+/* const result = confirm("Are you here?"); */
+/* console.log(result); */
+
+/* const answer = prompt("вам есть 18?", "18")
+console.log(answer); */
+
+/* const answers = [];
+
+answers[0] = prompt('what is your name?', '');
+answers[1] = prompt('what is your surname?', '');
+answers[2] = prompt('how old are you?', '');
+
+document.write(answers); */
+
+console.log('arr' + ' - object');
+console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+/* incr++;
+decr--; */
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
